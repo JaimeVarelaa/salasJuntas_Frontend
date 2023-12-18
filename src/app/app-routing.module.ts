ab38eb5c2c10e5f59ff36f4c2dcdf791fc78e074
JaimeVarelaa <jaimeadolfov31@gmail.com>
2023-12-17 23:58:16 -0600

    navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ReservacionesComponent } from './content/reservaciones/reservaciones.component';
+import { UsuariosComponent } from './content/usuarios/usuarios.component';
+import { SalasComponent } from './content/salas/salas.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'reservaciones', component: ReservacionesComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'salas', component: SalasComponent },
+  { path: '', redirectTo: '/reservaciones', pathMatch: 'full' },
+  { path: '**', redirectTo: '/reservaciones', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ReservacionesComponent } from './content/reservaciones/reservaciones.component';
+import { UsuariosComponent } from './content/usuarios/usuarios.component';
+import { SalasComponent } from './content/salas/salas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ReservacionesComponent,
+    UsuariosComponent,
+    SalasComponent
   ],
   imports: [
     BrowserModule,
